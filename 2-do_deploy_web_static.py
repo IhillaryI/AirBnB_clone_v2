65830833073c3452e2e84e7c87181ddd5aa9bfcc
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-09 02:39:31 +0000

    Modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os.path
-from datetime import datetime
-from fabric.api import local, cd, run, put, env
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
 env.hosts = ["18.207.207.66", "54.90.27.97"]
-env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """A fabric script that distributes an archive to a web server.
-
+    """Distributes an archive to a web server.
     Args:
-        archive_path (str): the path to the archive file
+        archive_path (str): The path of the archive to distribute.
     Returns:
-        False: if commands don't execute successfuly
-        True: on Successful command execution.
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
     if not os.path.isfile(archive_path):
         return False
-    file_name = os.path.relpath(archive_path).split("/")[1]
+    file_string = archive_path.split("/")[1]
+    file_name = file_string.split(".")[0]
 
     if put(archive_path, "/tmp", use_sudo=True).failed:
         return False
-    result = run(f"sudo tar xf /tmp/{file_name} -C"
+    result = run(f"sudo tar xf /tmp/{file_string} -C"
                  f" /data/web_static/releases/ --one-top-level")
     if result.failed:
         return False
-    if run(f"sudo rm /tmp/{file_name}").failed:
+    if run(f"sudo rm /tmp/{file_string}").failed:
         return False
-    if run("sudo rm -rf /data/web_static/current").failed:
+    if run(f"sudo rm -rf /data/web_static/current").failed:
         return False
     result = run(f"sudo ln -s /data/web_static/releases"
-                 f"/{file_name.split('.')[0]} /data/web_static/current")
+                 f"/{file_name} /data/web_static/current")
     if result.failed:
         return False
     return True
