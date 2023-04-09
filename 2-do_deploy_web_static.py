e87f353d3012b9a3f5da5622fcc0341597cfb70d
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-09 04:04:23 +0000

    Modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,31 +18,27 @@ def do_deploy(archive_path):
     """
     if os.path.isfile(archive_path) is False:
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    file_string = archive_path.split("/")[-1]
+    file_name = file_string.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if put(archive_path, "/tmp", use_sudo=True).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    result = run(f"sudo tar xf /tmp/{file_string} -C"
+                 f" /data/web_static/releases/ --one-top-level")
+    if result.failed:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if run(f"sudo rm /tmp/{file_string}").failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+    if run(f"sudo rm -rf /data/web_static/current").failed:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    result = run(f"sudo ln -s /data/web_static/releases"
+                 f"/{file_name} /data/web_static/current")
+    if result.failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+    result = run(f"sudo cp -r /data/web_static/releases/{file_name}/web_static/*"
+                 f" /data/web_static/releases/{file_name}")
+    if result.failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    if run(f"sudo rm -rf /data/web_static/releases/{file_name}/web_static").failed:
         return False
     return True
