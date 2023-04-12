d087271c449fe30b751533e18e818ad7a6a51825
Ifeanyichukwu Ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2023-04-12 20:43:15 +0000

    Modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+from os import path
+from datetime import datetime
+from fabric.api import local, run, env
+
+env.hosts = ["54.90.27.97", "18.207.207.66"]
+
+
+def do_pack():
+    """Create a tar archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = f"versions/web_static_{dt.year}{dt.month}" \
+            f"{dt.day}{dt.hour}{dt.minute}{dt.second}.tgz"
+
+    if local("mkdir -p versions").failed:
+        return None
+    if local(f"tar -czf {file} web_static").failed:
+        return None
+    return file
