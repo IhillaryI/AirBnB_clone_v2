ece63a65f70bf352ce7268329026c39e055139e4
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-12 01:39:06 +0000

    Modifeid

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,34 +23,34 @@ def do_pack():
     return file
 
 
-def do_deploy(archive_path):
-    """Deploys Archive to the provided hosts servers.
-    Args:
-        archive_path (str): path to the archive
-    Returns:
-        bool: True on success, otherwise False.
-    """
-    if not os.path.isfile(archive_path):
-        return False
-    archive = archive_path.split("/")[1]
-    file_name = archive.split(".")[0]
-
-    if put(archive_path, "/tmp").failed:
-        return False
-    if run(f"sudo tar xf /tmp/{archive} -C /data/web_static/releases/"
-            f" --one-top-level").failed:
-        return False
-    if run(f"sudo rm /tmp{archive}").failed:
-        return False
-    if run(f"sudo rm -rf /data/web_static/current").failed:
-        return False
-    if run(f"sudo ln -s /data/web_static/releases/{file_name}"
-            f" /data/web_static/current").failed:
-        return False
-    if run(f"sudo cp -r /data/web_static/releases/{file_name}/web_static/*"
-            f" /data/web_static/releases/{file_name}").failed:
-        return False
-    if run(f"sudo rm -rf"
-            f" /data/web_static/releases/{file_name}/web_static").failed:
-        return False
-    return True
+# def do_deploy(archive_path):
+#    """Deploys Archive to the provided hosts servers.
+#    Args:
+#        archive_path (str): path to the archive
+#    Returns:
+#        bool: True on success, otherwise False.
+#    """
+#    if not os.path.isfile(archive_path):
+#        return False
+#    archive = archive_path.split("/")[1]
+#    file_name = archive.split(".")[0]
+#
+#    if put(archive_path, "/tmp").failed:
+#        return False
+#    if run(f"sudo tar xf /tmp/{archive} -C /data/web_static/releases/"
+#            f" --one-top-level").failed:
+#        return False
+#    if run(f"sudo rm /tmp{archive}").failed:
+#        return False
+#    if run(f"sudo rm -rf /data/web_static/current").failed:
+#        return False
+#    if run(f"sudo ln -s /data/web_static/releases/{file_name}"
+#            f" /data/web_static/current").failed:
+#        return False
+#    if run(f"sudo cp -r /data/web_static/releases/{file_name}/web_static/*"
+#            f" /data/web_static/releases/{file_name}").failed:
+#        return False
+#    if run(f"sudo rm -rf"
+#            f" /data/web_static/releases/{file_name}/web_static").failed:
+#        return False
+#    return True
