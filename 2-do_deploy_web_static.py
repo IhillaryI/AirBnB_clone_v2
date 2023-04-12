6ace34fec2c4e7bac84bcd722c09699a276c4088
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-12 20:04:36 +0000

    modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" a Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy"""
 import os.path
 from datetime import datetime
 from fabric.api import local, env, run
