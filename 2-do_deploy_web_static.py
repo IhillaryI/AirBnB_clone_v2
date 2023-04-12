1a5dfb7e4afea149eccbf9c3a4d1fd747dc8e496
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-12 03:50:38 +0000

    modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,9 +2,9 @@
 # Fabfile to generates a .tgz archive from the contents of web_static.
 from os import path
 from datetime import datetime
-from fabric.api import local, put, env, run
+from fabric.api import local
 
-env.hosts = ["54.90.27.97", "18.207.207.66"]
+env.hosts = ["18.207.207.66", "54.90.27.97"]
 
 
 def do_pack():
@@ -21,38 +21,3 @@ def do_pack():
     if local("tar -cvzf {} web_static".format(file)).failed:
         return None
     return file
-
-
-def do_deploy(archive_path):
-    """Deploy a tar gzipped archive to remote web servers.
-    Args:
-        archive_path (str): the path to the archive
-    Returns:
-        bool: True if successful
-    """
-    if not path.isfile(archive_path):
-        return False
-    archive = archive_path.split("/")[1]
-    file_name = archive.split(".")[0]
-
-    if put(archive_path, "/tmp").failed:
-        return False
-    if run(f"sudo mkdir -p /data/web_static/releases/{file_name}").failed:
-        return False
-    if run(f"sudo tar xf /tmp/{archive} -C /data/web_static/releases/"
-            f"{file_name}").failed:
-        return False
-    if run(f"rm /tmp/{archive}").failed:
-        return False
-    if run(f"sudo rm -rf /data/web_static/current").failed:
-        return False
-    if run(f"sudo ln -s /data/web_static/releases/{file_name} "
-            f"/data/web_static/current").failed:
-        return False
-    if run(f"sudo cp -r /data/web_static/releases/{file_name}/web_static/*"
-            f" /data/web_static/releases/{file_name}").failed:
-        return False
-    if run(f"sudo rm -rf "
-            f"/data/web_static/releases/{file_name}/web_static").failed:
-        return False
-    return True
