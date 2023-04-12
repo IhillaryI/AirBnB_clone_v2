52e733c4b43b2a417b02cc935c061342ada7e779
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-12 01:52:10 +0000

    modified

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
-"""Module documents the deployment of an archive to a web servers"""
+# Fabfile to generates a .tgz archive from the contents of web_static.
+from os import path
+from datetime import datetime
+from fabric.api import local, put, env, run
+
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if local("mkdir -p versions").failed:
+        return None
+    if local("tar -cvzf {} web_static".format(file)).failed:
+        return None
+    return file
