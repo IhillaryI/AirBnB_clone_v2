d5fd02b5e2410f4ab07e427ccac9bc70cbf8b1cc
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-22 12:40:44 +0000

    Modify
    Adds the public getter method cities to the class

--- models/state.py
+++ models/state.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
+from models import storage
+from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
@@ -10,3 +13,14 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
+
+    @property
+    def cities(self):
+        """gets all cities related to this state"""
+        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+            city_objs = []
+            all_city = storage.all(City)
+            for k, v in all_city.items():
+                if v.state_id == self.id:
+                    city_objs.append(v)
+            return city_objs
