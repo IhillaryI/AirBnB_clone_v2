e5e9e11e585d75676748f1e966f9706ed3b8f19b
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-22 13:00:29 +0000

    Modified
    Modified the BaseModel class attribute
    Some code replacement in the __str__ method
    Rewrote the init method to fix errors that occur in 'db' mode

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+import os
 import uuid
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
@@ -7,34 +8,37 @@ from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
-Base.id = Column(String(60), nullable=False, primary_key=True)
-Base.created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-Base.updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        for key, value in kwargs.items():
-            if not hasattr(self, key):
-                setattr(self, key, value)
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for k, v in kwargs.items():
+                if k in ['updated_at', 'created_at']:
+                    setattr(self, k, datetime.fromisoformat(v))
+                if k != '__class__':
+                    setattr(self, k, v)
+                if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+                    if not kwargs.get('id'):
+                        setattr(self, 'id', str(uuid.uuid4()))
+                    if not kwargs.get('created_at'):
+                        setattr(self, 'created_at', datetime.utcnow())
+                    if not kwargs.get('updated_at'):
+                        setattr(self, 'updated_at', datetime.utcnow())
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        parse_dict = self.__dict__
+        cls = self.__class__.__name__
+        parse_dict = self.__dict__.copy()
         if parse_dict.get('_sa_instance_state'):
             del parse_dict['_sa_instance_state']
         return '[{}] ({}) {}'.format(cls, self.id, parse_dict)
@@ -42,7 +46,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
