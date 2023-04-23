995351ec0c88352221423c407b3c2cce1a7e61e1
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-23 21:11:49 +0000

    modified

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 states = list(storage.all(State).values())
 
+
 @app.route('/states_list', strict_slashes=False)
 def odd_or_even():
     """populates a html template with a list of states"""
-    print(states)
     return render_template('7-states_list.html', states=states)
 
 
-#@app.teardown_appcontext
-#def close():
- #   """remove the SQLAlchemy session after requests"""
-  #  storage.close()
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage instance"""
+    storage.close()
 
 
 if __name__ == '__main__':
