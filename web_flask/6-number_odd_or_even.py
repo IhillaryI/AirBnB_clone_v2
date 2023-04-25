59e32cda05b68ec051b67d767a5a01cc6428977f
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-25 02:23:46 +0000

    modified

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,13 +40,16 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """Populates and returns a html template file"""
-    return render_template('6-number_odd_or_even.html', value=n)
+    return render_template('5-number.html', value=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     """Populates and returns a html template file."""
-    return render_template('6-number_odd_or_even.html', value=n)
+    iseven = False
+    if n % 2 == 0:
+        iseven = True
+    return render_template('6-number_odd_or_even.html', value=n, iseven=iseven)
 
 
 if __name__ == '__main__':
